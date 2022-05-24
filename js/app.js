26fae00773924574f84474fcb0c096699e65b2c5
fadayopaul <paulfadayo@yahoo.com>
2022-05-24 23:59:56 +0100

    updated

--- js/app.js
+++ js/app.js
@@ -1,20 +1,24 @@
 let firstCard = 10;
-let secondCard = 11;
+let secondCard = 4;
 let sum = firstCard + secondCard;
 let hasBlackjack = false;
 let isAlive = true;
 let message = "";
 
 let messageEl = document.getElementById("message-el");
+let sumEL = document.querySelector("#sum-el");
+let cardsEL = document.querySelector("#cards-el");
 
 function startgame() {
+  cardsEL.textContent = "Cards: " + firstCard + " " + secondCard;
+  sumEL.textContent = "Sum: " + sum;
   if (sum <= 20) {
-    message = "Do you want to draw another card? 😄";
+    message = "Do you want to draw another card?";
   } else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🤯";
+    message = "You've got Blackjack!";
     hasBlackjack = true;
   } else {
-    message = "Sorry, you're out of the game! 😞";
+    message = "Sorry, you're out of the game!";
     isAlive = false;
   }
 
