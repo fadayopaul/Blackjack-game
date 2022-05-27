e2af331534df3bb56bf67429e970360819ea63db
fadayopaul <paulfadayo@yahoo.com>
2022-05-27 17:33:56 +0100

    Updated Javascript codes

--- js/app.js
+++ js/app.js
@@ -1,16 +1,28 @@
-let firstCard = 10;
-let secondCard = 4;
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
+let cards = [firstCard, secondCard]; //array- ordered list ot items
 let sum = firstCard + secondCard;
 let hasBlackjack = false;
 let isAlive = true;
 let message = "";
-
 let messageEl = document.getElementById("message-el");
 let sumEL = document.querySelector("#sum-el");
 let cardsEL = document.querySelector("#cards-el");
 
+function getRandomCard() {
+  return 5;
+}
+
 function startgame() {
-  cardsEL.textContent = "Cards: " + firstCard + " " + secondCard;
+  rendergame();
+}
+
+function rendergame() {
+  cardsEL.textContent = "Cards: ";
+  for (let i = 0; i < cards.length; i++) {
+    cardsEL.textContent += cards[i] + ", ";
+  }
+
   sumEL.textContent = "Sum: " + sum;
   if (sum <= 20) {
     message = "Do you want to draw another card?";
@@ -23,4 +35,15 @@ function startgame() {
   }
 
   messageEl.textContent = message;
+}
+
+function newcard() {
+  console.log("drawing a new card from the deck");
+  let card = getRandomCard();
+  sum += card;
+
+  cards.push(card);
+  console.log(cards);
+
+  rendergame();
 }
